469793b97e9e077ea2b7f6b89c94a90ee05a6d0c
creewick <ilia_Y@mail.ru>
2025-08-31 01:25:58 +0500

    progressive loading

--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -8,12 +8,12 @@ interface TokenProps {
 
 export default function Token({ icon, title, size = 90 }: TokenProps) {
   const style = { width: size, height: size };
-  const iconStyle = { backgroundImage: `url(${icon})`, top: title ? -5 : 0, ...style};
-  const textStyle = { fontSize: 16, fontWeight: 600, fontFamily: 'Dumbledore', fill: 'black', strokeWidth: 2, paintOrder: 'stroke', strokeOpacity: 0.5, stroke: 'white' };
+  const iconStyle = { top: title ? -5 : 0, ...style};
+  const textStyle = { fontSize: 16, fontWeight: 500, fontFamily: 'Dumbledore', fill: 'black', strokeWidth: 2, paintOrder: 'stroke', strokeOpacity: 0.5, stroke: 'white' };
 
   return (
     <div className='token' style={style}>
-      <span className='icon' style={iconStyle} />
+      <img className='icon' src={icon} loading='lazy' decoding="async"  style={iconStyle} />
       <svg viewBox="0 0 100 100">
         <text style={textStyle}>
           <textPath href='#circle-path' startOffset='50%' textAnchor='middle'>
--- src/pages/CharactersPage.tsx
+++ src/pages/CharactersPage.tsx
@@ -1,8 +1,11 @@
+import { useState, useEffect } from "react";
 import Button from "../components/Button";
 import Token from "../components/Token";
 import characters from '../data/characters/characters.json'
 
 export default function Home() {
+  const [visibleCharacters, setVisibleCharacters] = useState<typeof characters>([]);
+
   const renderButton = (icon: string, size: number, title?: string) => 
     <Button key={icon}>
       <Token icon={icon} size={size} title={title} />
@@ -17,6 +20,24 @@ export default function Home() {
     { icon: 'icons/fabled.webp', size: 40 },
   ];
 
+  useEffect(() => {
+    const batchSize = 1;
+    let currentIndex = 0;
+
+    const loadNextBatch = () => {
+      const nextBatch = characters.slice(currentIndex, currentIndex + batchSize);
+      setVisibleCharacters(prev => [...prev, ...nextBatch]);
+      currentIndex += batchSize;
+
+
+      if (currentIndex < characters.length) {
+        setTimeout(loadNextBatch, 0);
+      }
+    };
+
+    loadNextBatch();
+  }, []);
+
   return (
     <>
       <h1 className="title">Персонажи</h1>
@@ -24,7 +45,7 @@ export default function Home() {
         { buttons.map((button) => renderButton(button.icon, button.size)) }
       </div>
       <div style={{ display: 'flex', flexWrap: 'wrap', gap: '8px', contentVisibility: 'auto' }}>
-        { characters.map((character) => renderButton(`icons/${character.id}.webp`, 90, character.id)) }
+        { visibleCharacters.map((character) => renderButton(`icons/${character.id}.webp`, 90, character.id)) }
       </div>
     </>
   );
