867611519974f6696adc0996b33a0fe4241b686b
creewick <ilia_Y@mail.ru>
2025-09-10 11:37:06 +0500

    letter spacing on tokens

--- src/components/Token.tsx
+++ src/components/Token.tsx
@@ -12,12 +12,18 @@ export default function Token({ icon, title, grayscale = false, size = 90 }: Tok
   const base = import.meta.env.BASE_URL;
   const style = { width: size, height: size, filter: grayscale ? 'grayscale(100%)' : 'none' };
   const iconStyle = { top: title ? -5 : 0 };
+  const limit = 7;
+  const fontSize = !title
+    ? 16
+    : title.length < limit
+      ? 16
+      : 16 - Math.min(16, (title.length - limit) / 2);
 
   return (
     <div className='token' style={style}>
       <LazyLoadImage src={`${base}${icon}`} width={size} height={size} className='icon' style={iconStyle} />
       <svg className='text' viewBox="0 0 100 100">
-        <text fontSize={16} fontWeight={500} fill="black" strokeWidth={2} paintOrder="stroke" strokeOpacity={0.5} stroke="white">
+        <text fontSize={fontSize} fontWeight={500} fill="black" letterSpacing={0.5} strokeWidth={2} paintOrder="stroke" strokeOpacity={0.5} stroke="white">
           <textPath href='#circle-path' startOffset='50%' textAnchor='middle'>
             {title?.toUpperCase()}
           </textPath>
